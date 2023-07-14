714d57a2adbfcd377f1ab8d113e1ee1af267ec52
Radhika Gupte <gupter@oregonstate.edu>
2023-07-14 15:38:21 -0700

    updated home section

--- components/Main.jsx
+++ components/Main.jsx
@@ -6,45 +6,45 @@ import { FaGithub, FaLinkedinIn, FaPersonBooth } from 'react-icons/fa';
 
 const Main = () => {
   return (
-    <div id='home' className='w-full h-screen text-center'>
-      <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+    <div id="home" className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
-          <p className='uppercase text-sm tracking-widest text-gray-600'>
+          <p className="uppercase text-sm tracking-widest text-gray-600">
             LET&#39;S BUILD SOMETHING TOGETHER
           </p>
-          <h1 className='py-4 text-gray-700'>
-            Hi, I&#39;m <span className='text-[#5651e5]'> Radhika</span>
+          <h1 className="py-4 text-gray-700">
+            Hi, I&#39;m <span className="text-[#5651e5]"> Radhika</span>
           </h1>
-          <h1 className='py-2 text-gray-700'>A Back-End Web Developer</h1>
-          <p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
-            I’m focused on building scalable backend-end web applications
-            integrating with front-end technologies.
+          <h1 className="py-2 text-gray-700">A Fullstack Software Developer</h1>
+          <p className="py-4 text-gray-600 sm:max-w-[70%] m-auto">
+            I’m focused on building scalable fullstack web and mobile
+            applications.
           </p>
-          <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
             <a
-              href='https://www.linkedin.com/in/radhika-gupte/'
-              target='_blank'
-              rel='noreferrer'
+              href="https://www.linkedin.com/in/radhika-gupte/"
+              target="_blank"
+              rel="noreferrer"
             >
-              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
                 <FaLinkedinIn />
               </div>
             </a>
             <a
-              href='https://github.com/radgupte'
-              target='_blank'
-              rel='noreferrer'
+              href="https://github.com/radgupte"
+              target="_blank"
+              rel="noreferrer"
             >
-              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
                 <FaGithub />
               </div>
             </a>
             <a
-              href='https://drive.google.com/file/d/1HKg5bF34PWNO2R740eBCeS_qzcKf6pne/view?usp=share_link'
-              target='_blank'
-              rel='noreferrer'
+              href="https://drive.google.com/file/d/1ZGvLH_TluLiLsSu6EjYsowDcAaUfZYUY/view?usp=sharing"
+              target="_blank"
+              rel="noreferrer"
             >
-              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
                 <BsFillPersonLinesFill />
               </div>
             </a>
