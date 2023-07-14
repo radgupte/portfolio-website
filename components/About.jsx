0d3931e7e6c7dd9a7b0a08ab3ae185eab92c2bc5
Radhika Gupte <gupter@oregonstate.edu>
2023-07-14 15:20:34 -0700

    updated about section

--- components/About.jsx
+++ components/About.jsx
@@ -5,19 +5,21 @@ import AboutImg from '../public/assets/about.jpg';
 
 const About = () => {
   return (
-    <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
-      <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
-        <div className='col-span-2'>
-          <p className='uppercase text-xl tracking-widest text-[#5651e5]'>
+    <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+        <div className="col-span-2">
+          <p className="uppercase text-xl tracking-widest text-[#5651e5]">
             About
           </p>
-          <h2 className='py-4'>Who I Am</h2>
-          <p className='py-2 text-gray-600 text-2xl'>
-            I specialize in building full stack applications with a passion for backend development. 
-            I enjoy the process of learning new technologies and understand there is more than one way 
-            to accomplish a task and exploring the many possibilities is part of the process. Though I 
-            am most proficient in building  backend applications using Java, JavaScript and TypeScript. 
-            I am a fast learner and can pick up on new tech stacks as needed.
+          <h2 className="py-4">Who I Am</h2>
+          <p className="py-2 text-gray-600 text-2xl">
+            I specialize in building full stack applications with a passion for
+            backend development. I enjoy the process of learning new
+            technologies and understand there is more than one way to accomplish
+            a task and exploring the many possibilities is part of the process.
+            Though I am most proficient in building applications using Java,
+            Python, JavaScript and TypeScript, I am a fast learner and can pick
+            up on new tech stacks as needed.
           </p>
           {/* <p className='py-2 text-gray-600'>
             I started web developement in 2013 managing multiple e-commerce
@@ -27,14 +29,14 @@ const About = () => {
             time I run Code Commerce, a Youtube channel where I teach web
             developement and various front-end technologies.
           </p> */}
-          <Link href='/#projects'>
-            <p className='py-2 text-gray-600 underline cursor-pointer'>
+          <Link href="/#projects">
+            <p className="py-2 text-gray-600 underline cursor-pointer">
               Check out some of my latest projects.
             </p>
           </Link>
         </div>
-        <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-          <Image src={AboutImg} className='rounded-xl' alt='/' />
+        <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
+          <Image src={AboutImg} className="rounded-xl" alt="/" />
         </div>
       </div>
     </div>
